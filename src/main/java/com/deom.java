8362286945a0bb9543343f04b3024b48829bc784
layne <layne@atgui.com>
2023-01-30 11:32:41 +0800

    第一次

---
+++ src/main/java/com/deom.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @author 嗨嗨嗨
+ * @version ad
+ * @description asd
+ * @date 2023/1/30 11:25
+ */
+
+public class deom {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+    }
+}
